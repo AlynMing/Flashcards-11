9a5560bc95d128ec6b3ae89e059003dc2c758c73
Essence Cain <evcain1@gmail.com>
2020-02-20 19:37:18 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Essence Cain on 2/20/20.
+//  Copyright © 2020 Essence Cain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
